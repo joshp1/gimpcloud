05da914759b62b7a496e5052047a0000538874bd
Josh <joshp1@joshlucy.info>
2019-07-14 08:22:33 -0400

    Changed > to }

--- server/index.php
+++ server/index.php
@@ -14,7 +14,7 @@ Gimp Cloud
       <input type='text' name='un1' value="inter username" />
       <input type='password' name ='ps1' />
       <input type='submit' value = "Submit" />
-      <?>?>
+      <?}?>
     </form>
   </body>
 </html>
