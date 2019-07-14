a3ef217a856f3e15c11bdc903ee0b6ff4915bf50
joshp1 <joshp1@joshlucy.info>
2019-07-14 02:18:07 -0400

    CHanged

---
+++ server/index.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>
+Gimp Cloud
+    </title>
+  </head>
+  <body>
+    Color themes
+    <table border=0>
+      <tr>
+        <td>
+          <h1>
+Themes
+          </h1>
+          <span>
+Theme name
+          </span>
+          <span>
+Theme Color
+          </span>
+        </td>
+      </tr>
+    </table>
+  </body>
+</html>
