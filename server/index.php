ff88d60d814be1888e870023ea400585ec09077b
joshp1 <joshp1@joshlucy.info>
2019-07-14 08:14:44 -0400

    I need to quit using this in replace ofa FTP client

--- server/index.php
+++ server/index.php
@@ -4,12 +4,17 @@
 Gimp Cloud
     </title>
   </head>
-  <body>
+  <body><?php
     Color themes
+    if ($_COOKIE ['loginfo'])
+    {
+    print "loged N";
+    }else{?>
     <form action='verification.php' method = 'post'>
       <input type='text' name='un1' value="inter username" />
       <input type='password' name ='ps1' />
       <input type='submit' value = "Submit" />
+      <?>?>
     </form>
   </body>
 </html>
