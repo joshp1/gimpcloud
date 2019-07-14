3ee19c3ef7cbbb1e899dfe415adfce4e01495e9a
Josh <joshp1@joshlucy.info>
2019-07-14 13:47:51 -0400

    Working on site

--- server/index.php
+++ server/index.php
@@ -8,8 +8,26 @@ Gimp Cloud
     print 'Color themes';
     if ($_COOKIE ['loginfo'])
     {
-    print "loged N";
-    }else{?>
+      print "loged N";
+      $UserID= $_COOKIE ['UsrID'];
+
+      $cnnn = new mysqli ($servn, $uname, $pasw, $dbn);
+      if ($cnnn-> connect_error){
+        die ("Conection faild: " . $cnnn->connect_error);
+      }
+
+      $sql = "SELECT * FROM color WHERE UID = $UserID ORDER BY ID";
+      $res=$cnnn->query ($sql);
+
+      $res->num_rows >0){
+      while ($row=$res->fetch_assoc ()) {
+        echo $row ['theme_name'] . '<br />' . $row [theme_color];
+      } echo "<a"
+    } else {
+      print "Nothing in database. You need to add stuff first";
+    }
+    $cnnn->close ();
+    ?>
     <form action='verification.php' method = 'post'>
       <input type='text' name='un1' value="inter username" />
       <input type='password' name ='ps1' />
--- server/verification.php
+++ server/verification.php
@@ -9,21 +9,24 @@ if (!$select_db){
 }
 if (isset($_POST['un1']) and isset($_POST['ps1'])){
 
-// Assigning POST values to variables.
-$username = $_POST['un1'];
-$password = $_POST['ps1'];
+    // Assigning POST values to variables.
+   $username = $_POST['un1'];
+   $password = $_POST['ps1'];
 
-// CHECK FOR THE RECORD FROM TABLE
-$query = "SELECT * FROM `Main` WHERE username='$username' and Password='$password'";
+    // CHECK FOR THE RECORD FROM TABLE
+   $query = "SELECT * FROM `Main` WHERE username='$username' and Password='$password'";
 
-$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
-$count = mysqli_num_rows($result);
-
-if ($count == 1){
-$value= 'logged';
-setcookie ('loginfo', $value);
-header ($_SERVER ['HTTP_REFERER']);
-}else{echo "I guess not logeded in";
-}
-}
-?>
+   $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+   $count = mysqli_num_rows($result);
+   if ($_COOKIE ['loginfo']){
+     setcookie ('loginfo', '', time () - 3600);
+     setcookie ('UsrID', 'UID');
+     echo $result;
+   } else {
+     if ($count == 1){
+       $value= 'logged';
+       setcookie ('loginfo', $value);
+       header ($_SERVER ['HTTP_REFERER']);
+     }else{echo "I guess not logeded in";
+  }
+} }?>
