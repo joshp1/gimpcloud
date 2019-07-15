f40ef6ce48d1aaa6d7b0e45ba9bc07d75a2f8f0f
Josh <joshp1@joshlucy.info>
2019-07-15 13:39:39 -0400

    aded jquery URL

--- server/index.php
+++ server/index.php
@@ -3,6 +3,8 @@
     <title>
 Gimp Cloud
     </title>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script>
       function clipcolor (clr)
       { document.getElementById ()
