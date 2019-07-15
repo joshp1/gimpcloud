e96ee18874adf9dd1ffd53650eb1988aceb602cb
Josh <joshp1@joshlucy.info>
2019-07-15 00:35:36 -0400

    allining rectangles

--- server/index.php
+++ server/index.php
@@ -32,9 +32,9 @@ Gimp Cloud
         echo "<h1>". $row ['theme_name']. "</h1>";
         echo "<svg>";
         for ($x = 0;$x<$cnt; $x++){
-          $y=$x*10;
+          $y=$x*50;
           //echo "<span>".$rwo [$x]."</span>\n<span style='width:10px; height:10px;background-color:".$rwo [$x].";'></span>\n";
-          echo "<rect width='10' height='10' x='".$y."' y='10' style='fill:". $rwo [$x]. "' />";
+          echo "<rect width='50' height='50' x='".$y."' y='10' style='fill:". $rwo [$x]. "' />";
         }
        echo "</svg><br />\n<a href='verification.php'>";
      }
