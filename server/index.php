3c7537712f5c2bc9e905c36097f2a5aa24bfe7ac
Josh <joshp1@joshlucy.info>
2019-07-15 01:03:06 -0400

    fixed yet another eror

--- server/index.php
+++ server/index.php
@@ -40,7 +40,7 @@ Gimp Cloud
         for ($x = 0;$x<$cnt; $x++){
           $y=$x*50;
           //echo "<span>".$rwo [$x]."</span>\n<span style='width:10px; height:10px;background-color:".$rwo [$x].";'></span>\n";
-          echo "<rect width='50' height='50' x='".$y."' y='10' style='fill:". $rwo [$x]. "' onclick='clipcolor (".'"'.$rwo.'"'.")'/>\n";
+          echo "<rect width='50' height='50' x='".$y."' y='10' style='fill:". $rwo [$x]. "' onclick='clipcolor (".'"'.$rwo [$x].'"'.")'/>\n";
         }
        echo "</svg><br />\n<a href='verification.php'>";
      }
